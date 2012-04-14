006c02ca5ddf3a880e8fe81b3a42227687653ff4
Andrew Cutler <macropin@gmail.com>
2012-04-14 14:35:16 +1000

    Try fix deployment when included as a dep

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
 import glob
 
 setup(name='rng',
-    version='0.1',
+    version='0.1.1',
     url='https://github.com/macropin/random-name-generator',
     packages=['rng',],
     scripts=['name_generator.py',],
